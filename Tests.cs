00cdb008f879b5d10e284dfd2653cbbfdaeb3c2d
“Salil48” <salilchharia@gmail.com>
2023-02-09 18:40:45 +0530

    Retrieve Contacts In Time Range

--- Tests.cs
+++ Tests.cs
@@ -47,5 +47,23 @@ namespace AddressBookADO.NET_TEST
 
 
         }
+
+        /// <summary>
+        /// checking that we are getting the contacts based on the time range.
+        /// </summary>
+        [TestMethod]
+        public void CheckingForGettingContactDetailsInParticularTimeRange()
+        {
+            //creating list for expected output
+            List<AddressBookContactDetails> contactDetailsExpected = new List<AddressBookContactDetails>();
+            //adding data
+            contactDetailsExpected.Add(new AddressBookContactDetails { firstName = "Raj", lastName = "Verma", address = "Anbu Nagar", city = "tamilnadu", state = "Tn", zip = 635109, phoneNo = 8883380120, eMail = "raj.kce@gmail.com" });
+            //instatiating object for address book operations
+            AddressBookOperations addressBookOperations = new AddressBookOperations();
+            //getting actual contact list from address book operations-getting contact details from particular date range
+            List<AddressBookContactDetails> contactDetailsActual = addressBookOperations.GetAllContactDetailsForParticularDateRange();
+            //assert for comparing list
+            CollectionAssert.AreEqual(contactDetailsActual, contactDetailsExpected);
+        }
     }
 }
